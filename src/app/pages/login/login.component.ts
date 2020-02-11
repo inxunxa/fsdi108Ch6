f115c1a53143b3d77ba157dea126bb048dc283ac
Sergio Inzunza <sinzunza@authoritypartners.com>
2020-02-10 19:15:36 -0800

    fsdi 108 class 4

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,3 +1,4 @@
+import { DataService } from './../../services/data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userName = "";
+  password = "";
+  registeredUsers = [];
+
+  constructor(private data : DataService) {
+    this.registeredUsers = data.getUsers();
+  }
 
   ngOnInit() {
+  }
+
+  validateLogin(){
+    // travel the array
+    // compare user and password with each ele in the array
+    //  if find a match, login correct
+    for(var i = 0; i < this.registeredUsers.length; i++){
+      var user = this.registeredUsers[i];
+      if(user.userName == this.userName && user.password == this.password){
+        console.log("Credentials are correct, you are logged in!!");
+        return true;
+      }
+    }
+
+    console.log("Error, bad credentials!!");
   }
 
 }
--- src/app/pages/user-register/user-register.component.ts
+++ src/app/pages/user-register/user-register.component.ts
@@ -10,7 +10,8 @@ import { Component, OnInit } from '@angular/core';
 export class UserRegisterComponent implements OnInit {
  
   model: User = new User();
-  retypePassword = "";
+  retypePassword : string;
+  savedAlertVisible = false;
 
   constructor(private dataSrv : DataService) { }
 
@@ -22,5 +23,29 @@ export class UserRegisterComponent implements OnInit {
     
     // save the user (this.model) into a service
     this.dataSrv.saveUser(this.model);
+
+    // clear the form
+    this.model = new User();
+    this.retypePassword = null;
+    this.savedAlertVisible = true;
+  }
+
+  isPasswordStrong(){
+    var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return strongRegex.test(this.retypePassword);
+  }
+
+  isDataCorrect(){
+
+    // validate for password strongness    
+    if(!this.isPasswordStrong()){
+      return true; // <- button will be disabled
+    }
+      
+    return !this.model.email 
+    || !this.model.userName 
+    || !this.model.password 
+    || this.model.password.length < 6
+    || this.model.password != this.retypePassword;
   }
 }
